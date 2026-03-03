a47c529bd846603696eff301c2fe846d63473ade
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2026-03-03 15:16:59 -0300

    add projeto

---
+++ Exercicio1/src/exercicio1/Exercicio1.java
@@ -0,0 +1,30 @@
+
+package exercicio1;
+
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        
+      
+
+        Produto produto = new Produto();
+
+        produto.setNome("Notebook");
+        produto.setPreco(3500.00);
+
+        System.out.println("Nome: " + produto.getNome());
+        System.out.println("Preço: R$ " + produto.getPreco());
+    }
+}
+        
+        
+        
+        
+        
+        
+        
+     
+    
+   
+
